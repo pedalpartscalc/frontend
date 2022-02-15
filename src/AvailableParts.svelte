89564c4bfc3bc325c8aee5b859005a0b69285dd5
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-15 18:16:37 -0500

    use proper flexbox list for parts

--- src/AvailableParts.svelte
+++ src/AvailableParts.svelte
@@ -13,12 +13,48 @@
   });
 </script>
 
-{#if parts}
-  {#each parts as part}
-    <div class="container mx-auto">
-      <Part {part} />
+<div class="container py-4">
+  {#if parts}
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div
+            class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+          >
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >Component Name</th
+                  >
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >Component Type</th
+                  >
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >Quantity</th
+                  >
+                  <th scope="col" class="relative px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                {#each parts as part}
+                  <Part {part} />
+                {/each}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  {/each}
-{:else}
-  <p class="loading">loading...</p>
-{/if}
+  {:else}
+    <p class="loading">loading...</p>
+  {/if}
+</div>
--- src/Part.svelte
+++ src/Part.svelte
@@ -3,4 +3,19 @@
   export let part;
 </script>
 
-<p>{part.part_name}: Quantity {part.quantity}</p>
+<tr>
+  <td class="px-6 py-4 whitespace-nowrap">
+    <div class="text-sm font-medium text-gray-900">
+      {part.part_name}
+    </div>
+  </td>
+  <td class="px-6 py-4 whitespace-nowrap">
+    <div class="text-sm text-gray-900">{part.part_kind}</div>
+  </td>
+  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    {part.quantity}
+  </td>
+  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+    <a href="/" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+  </td>
+</tr>
