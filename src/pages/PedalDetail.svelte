03bf1c7c43df7ea80642dcea3edf38f78475d970
Jonathan Bayless <jonathanbayless@curative.com>
2022-03-27 20:58:57 -0400

    add param to route

---
+++ src/pages/PedalDetail.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { getPedal } from "../services/api";
+
+  export let pedalId;
+  let pedal;
+
+  const fetchPedal = async () => {
+    console.log(pedalId);
+    pedal = await getPedal(pedalId);
+  };
+
+  onMount(fetchPedal);
+</script>
+
+<p>Hello!</p>
