1342fa7ba8670a73f38029a2228d139fc9c44bd9
Jonathan Bayless <jonathanbayless@curative.com>
2022-03-30 19:14:51 -0400

    add the build_doc_link to the available pedals page

--- src/components/pedals/AvailablePedal.svelte
+++ src/components/pedals/AvailablePedal.svelte
@@ -16,4 +16,7 @@
   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
     {pedal?.required_parts?.length ?? 0}
   </td>
+  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+    {pedal?.build_doc_link ?? ""}
+  </td>
 </tr>
