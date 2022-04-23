a4a7794b0b5474c0c3e2ff7974c371d126e99ce8
Jonathan Bayless <jonathanbayless@curative.com>
2022-04-22 21:31:45 -0400

    basic logic for saving the parts

--- src/components/lib/Checkbox.svelte
+++ src/components/lib/Checkbox.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import { getContext } from "svelte";
+
   export let value = "";
+  export let name;
+  const { onBlur } = getContext("form");
 
   const classes =
     "w-6 h-6 mx-4 rounded border-2 border-gray-700 focus:ring-transparent";
 </script>
 
-<input type="checkbox" class={classes} bind:value />
+<input type="checkbox" {name} class={classes} bind:value on:blur={onBlur} />
--- src/components/pedals/BuildPedalModal.svelte
+++ src/components/pedals/BuildPedalModal.svelte
@@ -10,7 +10,24 @@
     modalBuildPedal.set(null);
   };
 
-  const onSubmit = (data) => {};
+  const onSubmit = (e: any) => {
+    const data = e?.detail?.data;
+
+    const quant_bought = {};
+    for (const [k, v] of Object.entries(data)) {
+      const matches = k.match(/\d+/);
+      if (!matches) continue;
+      const partNum = matches[0];
+
+      const partKey = k.replace("_" + partNum, "");
+      if (partKey === "bought") {
+        // the checkbox is checked, now get the quantity
+        quant_bought[partNum] = data["quant_" + partNum];
+      }
+    }
+
+    // TODO: for each of the quant bought, add a new part to the parts box (or update the quantity of the existing ones)
+  };
 
   let form = {
     name: {
@@ -24,12 +41,12 @@
 
 <Modal id="build_pedal_parts">
   {#if $modalBuildPedal}
-    <div class="flex flex-col gap-4 content-center w-full">
-      <h1 class="text-xl mx-auto">
-        Build Checklist for "{$modalBuildPedal.name}"
-      </h1>
-      <div>
-        <Form {form} on:submit={onSubmit}>
+    <Form {form} on:submit={onSubmit}>
+      <div class="flex flex-col gap-4 content-center w-full">
+        <h1 class="text-xl mx-auto">
+          Build Checklist for "{$modalBuildPedal.name}"
+        </h1>
+        <div>
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
@@ -61,16 +78,18 @@
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
-              {#each $modalBuildPedal.short_parts as part, i}
+              {#each $modalBuildPedal.short_parts as part}
                 <tr>
                   <td class="px-6 py-4 whitespace-nowrap">{part.part_name}</td>
                   <td class="px-6 py-4 whitespace-nowrap">{part.part_kind}</td>
                   <td class="px-6 py-4 whitespace-nowrap">{part.quantity}</td>
-                  <td class="px-6 py-4 whitespace-nowrap"><Checkbox /></td>
+                  <td class="px-6 py-4 whitespace-nowrap"
+                    ><Checkbox name={`bought_${part.id}`} /></td
+                  >
                   <td class="px-6 py-4 whitespace-nowrap"
                     ><Input
                       type="number"
-                      name={`quant_${i}`}
+                      name={`quant_${part.id}`}
                       value={part.quantity}
                     /></td
                   >
@@ -78,18 +97,19 @@
               {/each}
             </tbody>
           </table>
-        </Form>
+        </div>
+        <hr class="m-2" />
+        <div class="flex flex-row gap-4 w-full mx-auto justify-around">
+          <Button color="red-400" on:click={onClose}>Close</Button>
+          <Button
+            color="gray-400"
+            on:click={() => {
+              window.location.replace(`/pedals/${$modalBuildPedal.id}`);
+            }}>View Pedal Details</Button
+          >
+          <Button color="blue-500" type="submit">Save New Parts</Button>
+        </div>
       </div>
-      <hr class="m-2" />
-      <div class="flex flex-row gap-4 w-full mx-auto justify-around">
-        <Button color="gray-400" on:click={onClose}>Close</Button>
-        <Button
-          color="blue-500"
-          on:click={() => {
-            window.location.replace(`/pedals/${$modalBuildPedal.id}`);
-          }}>View Pedal Details</Button
-        >
-      </div>
-    </div>
+    </Form>
   {/if}
 </Modal>
